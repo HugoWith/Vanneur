c06f15466d07d2d2339d6cb00c2ca1459edff9dc
Marine Diot <marine.diot@hotmail.fr>
2019-11-11 17:47:58 +0100

    ajout des controllers et des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  resources :dashboards, only: [:index, :new, :create]
+  # get 'rentals/new', to: "rentals#new"
+  get 'vans/search', to: "vans#search"
+
+  resources :vans, only: [:show, :new, :create, :index] do
+    resources :rentals, only: [:new, :create]
+  end
+  # get 'rentals/create', to: "rentals#create"
+  # get 'vans/new' to: "vans#new"
+  # get 'vans/create', to: "vans#create"
+  # get 'vans/show' to: "vans#show"
   devise_for :users
-  root to: 'pages#home'
+
+  root to: 'vans#search'
+  # devise_scope :user do
+  #  root to: "vans#search"
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
