6e440e3e4748b05c0a11900b0ed32fabf1cd0972
Alex <alexou73@gmail.com>
2019-11-11 22:43:24 +0100

    "update"

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,21 +1,29 @@
 class DashboardsController < ApplicationController
 
+
 def new
     @van = Van.new
 end
 
+def index
+  @vans = Van.all  
+end
+
 def create
-    @van = Van.new(cocktail_params)
-    if @van.save
-      redirect_to dashboards_path
+  @user = current_user.id
+  @van = Van.new(vans_params)
+  @van.owner = @owner
+
+    if @van.save!
+      redirect_to dashboards_path(@owner)
     else
-      render :new
+      render 'dasboards/new'
     end
 end
 
 private
 
 def vans_params
-    params.require(:van).permit(:town, :pictures, :prices, :availability, :year, :kilometers, :hyppyness)
-  end
+    params.require(:van).permit(:town, :pictures, :description, :prices, :availability, :year, :kilometers, :hyppyness)
+end
 end
