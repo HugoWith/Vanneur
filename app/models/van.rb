b42e9d68018117d312f6f98dbcba45776770cb2e
Hugo Valla <Hugo_valla@hotmail.fr>
2019-11-12 18:34:09 +0100

    upload/ views picture

--- app/models/van.rb
+++ app/models/van.rb
@@ -7,4 +7,12 @@ class Van < ApplicationRecord
   validates :availability, presence: true
   validates :prices, presence: true
   validates :town, presence: true
+
+  def image
+    if pictures.present?
+      pictures
+    else
+      'https://images.unsplash.com/photo-1527786356703-4b100091cd2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80'
+    end
+  end
 end
