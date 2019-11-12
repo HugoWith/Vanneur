db94e32da047acf01966061b3fc957fee1bba38b
Alex <alexou73@gmail.com>
2019-11-12 10:54:46 +0100

    form Van ok

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,29 +1,15 @@
 class DashboardsController < ApplicationController
 
-
-def new
-    @van = Van.new
-end
+  def new
+    @vans
+  end
 
 def index
-  @vans = Van.all  
+  @vans = Van.all
 end
 
-def create
-  @user = current_user.id
-  @van = Van.new(vans_params)
-  @van.owner = @owner
 
-    if @van.save!
-      redirect_to dashboards_path(@owner)
-    else
-      render 'dasboards/new'
-    end
-end
+  def create
+  end
 
-private
-
-def vans_params
-    params.require(:van).permit(:town, :pictures, :description, :prices, :availability, :year, :kilometers, :hyppyness)
-end
 end
