35829748fd030b8c72faab67f835946a958d2fb8
Marine Diot <marine.diot@hotmail.fr>
2019-11-12 14:19:58 +0100

    ajout de la foreign key customer id pour qu'on ait bien owner id et customer id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+puts
+
+Van.destroy_all
+
+
+hugo = User.create!(
+  email: hugo@beteur.fr,
+  password:,
+  first_name:,
+  last_name: )
+
+alex = User.create!(
+  email: alex@beteur.fr,
+  password:,
+  first_name:,
+  last_name: )
+
+marin = User.create!(
+  email:,
+  password:,
+  first_name:,
+  last_name: )
+
+marine = User.create!(
+  email:,
+  password:,
+  first_name:,
+  last_name: )
+
+jojo = User.create!(
+  email:,
+  password:,
+  first_name:,
+  last_name: )
+
+kev = User.create!(
+  email:,
+  password:,
+  first_name:,
+  last_name: )
+
+van = Van.create(
+  town:,
+  description:,
+  prices:,
+  availability:,
+  hyppyness:,
+  year:,
+  kilometers:,
+  owner: )
