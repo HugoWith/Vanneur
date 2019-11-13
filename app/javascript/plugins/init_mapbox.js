b43e616b2841a4715cebb5ce2904239ce967ba89
Marine Diot <marine.diot@hotmail.fr>
2019-11-13 15:57:16 +0100

    la map est fittee

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,10 +1,9 @@
 import mapboxgl from 'mapbox-gl';
 
-
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 60, maxZoom: 15, duration: 0 });
 };
 
 const initMapbox = () => {
@@ -22,15 +21,15 @@ const initMapbox = () => {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
     });
+    fitMapToMarkers(map, markers);
   }
 };
 
-  // [ ... ]
 
+//     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }))
 
-// if (mapElement) {
-//   // [ ... ]
-//   fitMapToMarkers(map, markers);
-// }
+//   }
+// };
+
 
 export { initMapbox };
