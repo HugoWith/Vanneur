3b1f801d6255efa01c5524bf53f6cb84013ca6c1
Hugo Valla <Hugo_valla@hotmail.fr>
2019-11-14 19:47:13 +0100

    dashboard index

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,4 +4,12 @@ class DashboardsController < ApplicationController
     # on devra differencier dans la vue le dashboard locataire et proprietaire,
     # en gerant le cas du proprietaire locataire
   end
+
+  def owned
+    @owned_vans = current_user.vans
+  end
+
+  def rented
+    @rented_vans = current_user.rented_vans
+  end
 end
