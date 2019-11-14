000b97e0ada469056bf878b967c9e9945a9ce40d
marin <vanthiermarin@orange.fr>
2019-11-14 13:06:49 +0100

    fixe Algolia + require ujs

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -4,11 +4,12 @@ const initAutocomplete = () => {
   const addressInput = document.getElementById('van_town');
   if (addressInput) {
     places({ container: addressInput });
+    const options = {
+        type: 'city'
+    }
+    const placesInstance = places(options)
   }
-  const options = {
-      type: 'city'
-  }
-  const placesInstance = places(options)
+
 };
 
 export { initAutocomplete };
